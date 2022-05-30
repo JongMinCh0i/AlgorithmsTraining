82ad6fc452baf03e736cfba4bf73c397f1725f26
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-05-30 10:22:56 +0900

    [Silver III] Title: 스택 수열, Time: 812 ms, Memory: 99288 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,4 +1,16 @@
 import java.io.*;
+
+
+/**
+ *  해당 문제의 주요한 키 포인트는  [오름차순, 1부터 n 까지의 수, ]
+ *  스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다는 점에 있다.
+ *  또한 해당 stack에 push하는 순서가 반드시 오름차순을 지켜야 한다는 점.
+ *  임의의 수열이 주어졌을 때 이 스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면
+ *  어떠한  순서로  push, pop 연산을 수행해야 하는지를 알아 낼 수 있다고 한다.
+ *
+ *
+ *
+ */
 import java.util.*;
 
 public class Main {
@@ -8,18 +20,18 @@ public class Main {
 
         int N = sc.nextInt();
         int[] answer = new int[N];
-        Stack<Integer> stk = new Stack<>(); // stk의 존재이유?
+        Stack<Integer> stk = new Stack<>();
 
         for (int i = 0; i < N; i ++) {
             answer[i] = sc.nextInt();
         }
-        
+
         int num = 1;
         StringBuffer sb = new StringBuffer();
         boolean result = true;
 
         for (int i = 0; i < answer.length; i++) {
-            int su = answer[i];
+            int su = answer[i]; // 현재 0번째 수열 위치
             if (su >= num) {
                 while (su >= num) {
                     stk.push(num++);
