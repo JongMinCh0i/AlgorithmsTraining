138ca1d882de9e8725f3abe39b59c89c5d68317c
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-06-03 23:30:45 +0900

    [Bronze V] Title: 검증수, Time: 76 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = 0;
+        for (int i = 0; i < 5; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            N += temp * temp;
+        }
+        int answer = N % 10;
+        
+        System.out.println(answer);
+    }
+}
