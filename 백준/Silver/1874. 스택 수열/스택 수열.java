19fd734ad726c0cc0cc18bcaa30bea7ab5de6af2
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-06-04 12:43:57 +0900

    Update 스택 수열.java

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,4 +1,4 @@
-/**
+/*
  * 해당 문제의 주요한 키 포인트는  [오름차순, 1부터 n 까지의 수, ]
  * 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다는 점에 있다.
  * 또한 해당 stack에 push하는 순서가 반드시 오름차순을 지켜야 한다는 점.
@@ -6,7 +6,6 @@
  * 어떠한  순서로  push, pop 연산을 수행해야 하는지를 알아 낼 수 있다고 한다.
  * boolean true value 실행 조건 설정
  * 현재 수열 값 >= 자연수
- * <p>
  * 현재 수열 값 < 자연수
  */
 
@@ -16,42 +15,42 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         Stack<Integer> stk = new Stack<>();
         StringBuffer sb = new StringBuffer();
 
-        int N = Integer.parseInt(br.readLine());
+        boolean condition = true;
 
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        boolean condition = true;
-        int num = 1;
+        int su = 1;
 
-        for (int i = 0; i < arr.length; i++) {
-            int su = arr[i];
-            if (su >= num) {
-                while (su >= num) {
-                    stk.push(num++);
-                    sb.append("+ \n");
+        for (int num : arr) {
+            if (num >= su) {
+                while (num >= su) {
+                    stk.push(su++);
+                    sb.append("+\n");
                 }
                 stk.pop();
-                sb.append("- \n");
-            } else {
-                int n = stk.pop();
-                if (n > su) {
+                sb.append("-\n");
+            } else { // num(arr의 값) < su(1부터 증가하는 순번)
+                int top = stk.pop(); // stk의 값은 순차적으로 + 1 씩 커지므로 해당 상황에서 top은 해당 배열의 최대치다.
+                if (top > num) {
                     System.out.println("NO");
                     condition = false;
                     break;
-                } else if (n <= su) {
+                } else { // top <= num : 상위의 값이 num(arr[i])과 같거나 작을 경우 - 처리
+//                    stk.pop(); :
                     sb.append("-\n");
                 }
+
             }
         }
-        if (condition)
-            System.out.println(sb.toString());
+            if (condition) // 주석 처리 할 경우 BOJ 출력 초과 
+                System.out.println(sb);
     }
 }
