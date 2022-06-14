59106cffeac2a1a0d763aefca82030f3aeaf3beb
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-06-15 01:57:19 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i));
+            if (i % 10 == 9) {
+                System.out.println();
+            }
+        }
+    }
+}
+
