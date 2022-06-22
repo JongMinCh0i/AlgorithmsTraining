24272f12c63235d070deebac51d4de41abac97cb
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-06-22 13:34:58 +0900

    [Silver V] Title: K번째 수, Time: 3328 ms, Memory: 668040 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int index = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < A; i++) {
+            arr.add(Integer.valueOf(st.nextToken()));
+        }
+
+        Collections.sort(arr);
+
+        System.out.println(arr.get(index - 1));
+    }
+}
