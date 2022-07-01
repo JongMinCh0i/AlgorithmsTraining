2fca555b21b53b089b3b1b1107d5f400a02dacd9
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-07-01 16:42:14 +0900

    [Silver V] Title: 숫자 카드, Time: 928 ms, Memory: 141488 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -4,45 +4,44 @@ import java.util.*;
 public class Main {
     static int N, M;
     static int[] A;
+    static StringBuilder sb;
 
-    static int findNum(int[] A, int key) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        A = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A, 1, N + 1);
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        // 1부터 M까지 이분탐색 실행
+        for (int i = 1; i <= M; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            findNum(A, n);
+        }
+        System.out.println(sb);
+    }
+    // 이분탐색 코드
+    static void findNum(int[] A, int key) {
         int L = 1;
         int R = A.length - 1;
-
         while (L <= R) {
             int mid = (L + R) / 2;
             if (key == A[mid]) {
-                return 1;
+                sb.append("1").append(" ");
+                return;
             } else if (A[mid] < key) {
                 L = mid + 1;
             } else if (A[mid] > key) {
                 R = mid - 1;
             }
         }
-        return 0;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        A = new int[N + 1];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(A, 1, N + 1);
-
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 1; i <= M; i++) {
-            int n = Integer.parseInt(st.nextToken());
-            System.out.print(findNum(A, n) + " ");
-        }
+        sb.append("0").append(" ");
     }
 }
