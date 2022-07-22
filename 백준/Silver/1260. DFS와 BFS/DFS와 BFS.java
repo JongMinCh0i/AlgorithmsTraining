d9d2c8ac49103c361fcde0c25fe967d7dc1ed415
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-07-22 20:33:10 +0900

    [Silver II] Title: DFS와 BFS, Time: 180 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,41 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, V;
+    static int V, E, start;
     static boolean[] visit;
-    static ArrayList<Integer>[] arrayLists;
+    static ArrayList<Integer>[] arr;
     static StringBuilder sb;
 
-    public static void dfs(int i) {
-        visit[i] = true; // 해당 i 방문 처리
-        sb.append(i).append(' ');
+    static void dfs(int x) {
+        visit[x] = true;
+        sb.append(x).append(' ');
 
-        for (int y : arrayLists[i]) { // y 가 arrLists[i]를 전부 탐색한다.
-            if (visit[y]) {           // 그 과정에서 y가 이미 방문했었더라면
-                continue;             // continue; 다시 반목문으로 돌아간다.
+        for (int y : arr[x]) {
+            if (visit[y]) {
+                continue;
             }
-            dfs(y);                   // dfs(y) 재귀 처리
+            dfs(y);
         }
     }
 
-    public static void bfs(int strat) {
+    static void bfs(int start) {
         Queue<Integer> queue = new LinkedList<>();
 
-        // enqueue 시 visit 처리
-        queue.add(strat);
-        visit[strat] = true;
+        queue.add(start);
+        visit[start] = true;
 
-        // queue 가 비어있다 = 탐색 완료 / queue의 사이즈가 1 이상이다.= 탐색 중
-        while (!queue.isEmpty()) { // 더 이상 확인 할 점이 없다면 정지
+        while (!queue.isEmpty()) {
             int x = queue.poll();
 
             sb.append(x).append(' ');
 
-            for (int y : arrayLists[x]) {
-                // 이미 방문했다면 continue 처리
-                if (visit[y]) continue;
+            for (int y : arr[x]) {
+                if (visit[y])
+                    continue;
 
-                // enqueue 시 visit 처리
                 queue.add(y);
                 visit[y] = true;
             }
@@ -48,43 +45,36 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
-        arrayLists = new ArrayList[N + 1];
-        visit = new boolean[N + 1];
+        V = Integer.parseInt(st.nextToken());   // V
+        E = Integer.parseInt(st.nextToken());   // E
+        start = Integer.parseInt(st.nextToken());   // Start
+        arr = new ArrayList[V + 1];
+        visit = new boolean[V + 1];
 
-        // 각각의 노드 수 만큼 리스트 생성 1 부터 시작하기에 초기 idx = 1
-        for (int i = 1; i <= N; i++) {
-            arrayLists[i] = new ArrayList<>();
+        for (int i = 1; i <= V; i++) {
+            arr[i] = new ArrayList<>();
         }
 
-        // input
-        // 인정리스트를 만든다.
-        for (int i = 0; i < M; i++) {
-            // ex) 1 2 : 1번 노드가 2번 노드와 연결된다고 했을 때
-            // 2번 노드 또한 1번 노드와 연결된다는 것이 성립한다.
-            // 즉 arr[1] = 2 /  arr[2] = 1 성립한다는 것
 
+        for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            arrayLists[x].add(y);
-            arrayLists[y].add(x);
+            arr[x].add(y);
+            arr[y].add(x);
         }
 
-        for (int i = 1; i <= N; i++) {
-            Collections.sort(arrayLists[i]);
+        for (int i = 1; i <= V; i++) {
+            Collections.sort(arr[i]);
         }
 
-        dfs(V);
+
+        dfs(start);
         sb.append('\n');
-        // visit 배열 초기화
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= V; i++) {
             visit[i] = false;
         }
-        bfs(V);
+        bfs(start);
         System.out.println(sb);
     }
 }
-
