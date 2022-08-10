0f4852f6a2346cbb24f8ac7f25270eb6dde48ab0
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-08-10 20:41:47 +0900

    [Bronze V] Title: 엔드게임 스포일러, Time: 72 ms, Memory: 11272 KB -BaekjoonHub

---
+++ 백준/Bronze/17295. 엔드게임 스포일러/엔드게임 스포일러.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static String str;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        logic();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+    }
+
+    static void logic() {
+        System.out.println("Avengers: Endgame");
+
+    }
+}
+
