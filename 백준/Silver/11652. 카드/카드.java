2119970742fed39db5dcb6e1f61fc9487bcea09e
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-08-16 17:59:15 +0900

    [Silver IV] Title: 카드, Time: 308 ms, Memory: 24616 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -2,35 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br;
+    static int N;
+    static long[] arrLong;
+    static int modeCnt, tempCnt;
+    static long mode;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        input();
+        logic();
+    }
 
-        long[] arr = new long[N + 1];
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Long.parseLong(br.readLine());
+    static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arrLong = new long[N];
+
+        for (int i = 0; i < N; i++) {
+            arrLong[i] = Long.parseLong(br.readLine());
         }
+    }
 
-        // 첫 번째 최빈값은 값이 진행 되는 순간 해당 값
-        // int mode로 값을 표현할 경우 해당 문제의 범위에서 overflow 및 underflow 발생
-        Arrays.sort(arr, 1, N + 1);
-        
-        long mode = arr[1];
-        // 최빈값이 등장했던 횟수
-        long modeCnt = 1;
-        //  현재 숫자의 등장 횟수
-        long currentCnt = 1;
+    static void logic() {
+        Arrays.sort(arrLong);
+        mode = arrLong[0];
+        modeCnt = 1;
+        tempCnt = 1;
 
-
-        for (int i = 2; i <= N; i++) {
-            if (arr[i] == arr[i - 1]) {
-                currentCnt++;
-            } else {
-                currentCnt = 1;
+        for (int i = 1; i < arrLong.length; i++) {
+            if (arrLong[i] == arrLong[i - 1]) {
+                modeCnt++;
+            } else if (arrLong[i] != arrLong[i - 1]) {
+                modeCnt = 1;
             }
-            if (modeCnt < currentCnt) {
-                modeCnt = currentCnt;
-                mode = arr[i];
+
+            if (modeCnt > tempCnt) {
+                tempCnt = modeCnt;
+                mode = arrLong[i];
             }
         }
         System.out.println(mode);
