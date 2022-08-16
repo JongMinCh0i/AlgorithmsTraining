8ee25d38ee83f95d919283dd5c172c0ab5c12b90
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-08-16 18:02:12 +0900

    Update 카드.java

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,7 +1,9 @@
+package PS;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Boj11652 {
     static BufferedReader br;
     static int N;
     static long[] arrLong;
@@ -35,7 +37,10 @@ public class Main {
             } else if (arrLong[i] != arrLong[i - 1]) {
                 modeCnt = 1;
             }
-
+            
+            // 초기 1회는 temp에 modeCnt 저장됨 
+            // 이후 저장된 modeCnt이후 값의 비교 
+            // 기존보다 많은 cnt의 경우 mdoe 값 변경
             if (modeCnt > tempCnt) {
                 tempCnt = modeCnt;
                 mode = arrLong[i];
