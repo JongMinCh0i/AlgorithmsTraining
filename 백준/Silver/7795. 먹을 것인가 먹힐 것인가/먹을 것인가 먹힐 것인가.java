ab230e373a01ccc049e3146cbf50a77e646886c9
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-08-23 17:27:24 +0900

    Update 먹을 것인가 먹힐 것인가.java

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,45 +1,71 @@
+package PS;
+
+// 내가 이진탐색 돌릴 배열을 우선 정렬하고 이분탐색을 실행한다.
+// LowerBound를 실행한 이유는 내가 목표로 잡은 값 이하의 수가 통과한다면 이미 정렬된 배열에선 해당 통과한 값 보다 작은 값은
+// 반드시 통과 될 것을 알고 있기 때문이다.
+// 이분 탐색을 돌릴 해당 배열은 우선 정렬되어 있어야 한다. 정렬 범위의 경우 1부터 값을 넣었기에 정렬 범위 또한 지정해서 실행해야한다.
+// 가급적 가능하다면 배열의 길이를 범위로 잡지 말 것
+
 import java.io.*;
 import java.util.*;
 
-class Main {
-    static int[] N, M;
-    static int i, j, T;
+class Boj7795 {
+    static int TC, N, M;
     static BufferedReader br;
     static StringTokenizer st;
+    static StringBuilder sb;
+    static int[] A, B;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
+        TC = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= T; i++) {
+        for (int i = 0; i < TC; i++) {
             input();
             logic();
         }
     }
 
     static void input() throws IOException {
+        sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
-        i = Integer.parseInt(st.nextToken());
-        j = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        N = new int[i + 1];
-        M = new int[j + 1];
+        A = new int[N + 1];
+        B = new int[M + 1];
 
         st = new StringTokenizer(br.readLine());
-        for (int k = 1; k <= i; k++) {
-            N[k] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            A[i] = num;
         }
 
         st = new StringTokenizer(br.readLine());
-        for (int k = 1; k <= j; k++) {
-            M[k] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            B[i] = num;
         }
     }
 
-    static int lower_bound(int[] arr, int L, int R, int X) {
-        // arr[L...R]에서 X 미만의 수 중 제일 오른쪽 인덱스를 return 하는 함수
-        // ex) 11,12,34,53 x = 34 , 34미만의 수 11, 12 중 12 리턴
-        // 목표 결괏값의 L - 1  == 왼쪽미만에서 가장 오른쪽
+    static void logic() throws IOException {
+        int ans = 0;
+
+        Arrays.sort(B, 1, M + 1);
+
+        for (int i = 1; i <= N; i++) {
+            // 이분탐색을 실행할 배열 B 배열 L 은 1부터 시작 , R은 M까지 시작
+            // 비교할 값은 A[i]로 선택
+            ans += lowerBound(B, 1, M, A[i]);
+        }
+
+        System.out.println(ans);
+    }
+
+    // 정렬되어 있는 대상 배열 int[] arr
+    // L은 arr의 시작 지점
+    // R은 arr의 끝
+    static int lowerBound(int[] arr, int L, int R, int X) {
         int result = L - 1;
 
         while (L <= R) {
@@ -52,18 +78,5 @@ class Main {
             }
         }
         return result;
-    }
-
-    static void logic() {
-        // M 번 배열의 1번 인데스 부터 J + 1 // 끝까지 정렬한다.
-        // 중요 1부터 시작한다.
-        Arrays.sort(M, 1, j + 1);
-
-        int cnt = 0;
-        for (int k = 1; k <= i; k++) {
-            cnt += lower_bound(M, 1, j, N[k]);
-        }
-
-        System.out.println(cnt);
     }
 }
