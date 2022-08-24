84acd757e6f76882ab035dd412825495a9bc56e8
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-08-24 13:12:02 +0900

    [Bronze V] Title: Cupcake Party, Time: 76 ms, Memory: 11392 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        System.out.println(Math.abs(28 - ((N * 8) + (M * 3))));
+    }
+}
