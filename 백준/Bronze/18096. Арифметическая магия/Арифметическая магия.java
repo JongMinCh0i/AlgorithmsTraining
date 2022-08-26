e9013b9fd28004287376d9efaf7a6367f3a0af6d
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-08-26 19:43:30 +0900

    [Bronze V] Title: Арифметическая магия, Time: 76 ms, Memory: 11436 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, K;
+
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(1);
+        
+    }
+}
