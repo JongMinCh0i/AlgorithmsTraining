257290c106e31bf270437745684a4f17ec8879a0
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-09-09 15:42:31 +0900

    [Gold V] Title: 두 용액, Time: 296 ms, Memory: 31192 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,12 +1,18 @@
+// v1 = left v2 = right 가 된다.
+// v1 과 v2의 최적의 해는 -v1 일 것이다.
+// 해당 수가 0에 더 가깝기에 그것이 정해가 될 것
+// Hint : LowerBound(int[] arr, int L, int R, int X)
+// LowerBound(arr, L + 1, int R, arr[i] * -1) 해당 범위 설정의 이유는?
+
 import java.io.*;
 import java.util.*;
 
 class Main {
-    static int N;
-    static int[] arrInt;
     static BufferedReader br;
     static StringTokenizer st;
     static StringBuilder sb;
+    static int N, v1, v2;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         input();
@@ -17,42 +23,43 @@ class Main {
         sb = new StringBuilder();
         br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
         st = new StringTokenizer(br.readLine());
-
-        arrInt = new int[N];
-
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            arrInt[i] = num;
+            arr[i] = Integer.parseInt(st.nextToken());
         }
     }
 
     static void logic() {
-        Arrays.sort(arrInt);
-        int bestNum = Integer.MAX_VALUE;
-        int v1 = 0;
-        int v2 = 0;
+        Arrays.sort(arr);
+        v1 = 0;
+        v2 = 0;
+        int bigN = Integer.MAX_VALUE;
 
         for (int i = 0; i < N; i++) {
-            int cand = lower_Bound(arrInt, i + 1, N, arrInt[i] * -1);
+            int candidate = lowerBound(arr, i + 1, N, arr[i] * -1);
 
-            if (i < cand - 1 && Math.abs(arrInt[i] + arrInt[cand - 1]) < bestNum) {
-                bestNum = Math.abs(arrInt[i] + arrInt[cand - 1]);
-                v1 = arrInt[i];
-                v2 = arrInt[cand - 1];
+            if (i < candidate - 1 && Math.abs(arr[i] + arr[candidate - 1]) < bigN) {
+                bigN = Math.abs(arr[i] + arr[candidate - 1]);
+
+                v1 = arr[i];
+                v2 = arr[candidate - 1];
             }
-            
-            if (cand < N && Math.abs(arrInt[i] + arrInt[cand]) < bestNum) {
-                bestNum = Math.abs(arrInt[i] + arrInt[cand]);
-                v1 = arrInt[i];
-                v2 = arrInt[cand];
+
+            if (candidate < N &&  Math.abs(arr[i] + arr[candidate]) < bigN) {
+                bigN = Math.abs(arr[i] + arr[candidate]);
+                v1 = arr[i];
+                v2 = arr[candidate];
             }
         }
-        sb.append(v1).append(' ').append(v2);
+
+        sb.append(v1).append(" ").append(v2);
         System.out.println(sb);
     }
 
-    static int lower_Bound(int[] arr, int L, int R, int X) {
+
+    static int lowerBound(int[] arr, int L, int R, int X) {
         while (L < R) {
             int mid = (L + R) / 2;
             if (arr[mid] >= X) {
