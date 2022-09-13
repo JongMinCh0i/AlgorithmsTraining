127afc54db8a11835d7042b621a0b0e46999f685
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-09-13 18:31:42 +0900

    [Silver II] Title: 나무 자르기, Time: 680 ms, Memory: 175244 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 class Main {
     static int N, M;
-    static long[] arr;
-    static StringBuilder sb;
     static BufferedReader br;
     static StringTokenizer st;
+    static StringBuilder sb;
+    static int[] arrInt;
 
     public static void main(String[] args) throws IOException {
         input();
@@ -18,53 +18,43 @@ class Main {
         br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        arrInt = new int[N];
         M = Integer.parseInt(st.nextToken());
-
-        arr = new long[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            arrInt[i] = num;
         }
-
-        Arrays.sort(arr);
     }
 
-    // 정답인 H를 파리미터에 넣음
-    static boolean determination(int H) {
-        // H 높이로 나무들을 잘랐을 때, M 만큼을 얻을 수 있으면 true, 없으면 false 리턴
-        long sum = 0;
+    static boolean check(int H) {
+        long ans = 0;
         for (int i = 0; i < N; i++) {
-            // 잘리는 조건
-            if (arr[i] > H) {
-                // H 높이에 잘릴 경우 잘린 합을 sum에 합한다.
-                sum += arr[i] - H;
+            if (arrInt[i] > H) {
+                ans += arrInt[i] - H;
             }
         }
 
-        // 가능?
-        if (sum >= M) {
-            return true;
-        } else {
-            return false;
-        }
+        if (ans >= M)  return true;
+        else return false;
     }
 
     static void logic() {
-        long L = 1, R = arr[arr.length - 1], ans = 0;
-        // [L....R] 범위 안에 정답이 존재 한다.
-        // 이분 탐색과 determination 활용해서 answer 구하기
-
-
+        Arrays.sort(arrInt);
+        long L = 0;
+        int R = Integer.MAX_VALUE;
+        long X = 0;
         while (L <= R) {
-            int mid = (int) ((L + R) / 2);
-            if (determination(mid)) {
-                ans = mid;
+            int mid = (int) (L + R) / 2;
+            if (check(mid)) {
+                X = mid;
                 L = mid + 1;
             } else {
                 R = mid - 1;
             }
         }
-        System.out.println(ans);
+        System.out.println(X);
     }
 }
+
