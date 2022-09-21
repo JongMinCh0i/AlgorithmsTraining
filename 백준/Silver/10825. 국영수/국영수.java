2240e5afe94be74e2be700c0b7f390ecb95bd77a
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-09-21 19:47:16 +0900

    [Silver IV] Title: 국영수, Time: 612 ms, Memory: 63032 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,54 +1,69 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static StringBuilder sb;
+    static StringTokenizer st;
+    static BufferedReader br;
+    static int N;
+
+    static testForArr[] arr2;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        int A = Integer.parseInt(br.readLine());
-        // Score 배열을 A 만큼 생성
-        Score[] scores = new Score[A];
-        // Input
-        for (int i = 0; i < A; i++) {
+        input();
+        logic();
+    }
+
+    static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        arr2 = new testForArr[N];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            scores[i] = new Score(st.nextToken(), Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            String name = st.nextToken();
+            int Korean = Integer.parseInt(st.nextToken());
+            int English = Integer.parseInt(st.nextToken());
+            int Math = Integer.parseInt(st.nextToken());
+            arr2[i] = new testForArr(name, Korean, English, Math);
         }
 
-        // 정렬
-        Arrays.sort(scores);
-        for (int i = 0; i < A; i++) {
-            sb.append(scores[i].str).append('\n');
+        Arrays.sort(arr2); // 정렬
+    }
+
+    static void logic() {
+        for (int i = 0; i < N; i++) {
+            sb.append(arr2[i].name).append('\n');
         }
+
         System.out.println(sb);
     }
 
-    static class Score implements Comparable<Score> {
-        String str;
-        int langScore, mathScore, engScore;
 
-        // 초기화
-        public Score(String str, int langScore, int engScore, int mathScore) {
-            this.str = str;
-            this.langScore = langScore;
-            this.engScore = engScore;
-            this.mathScore = mathScore;
+    static class testForArr implements Comparable<testForArr> {
+        String name;
+        int Korean, English, Math;
+
+        public testForArr(String name, int Korean, int English, int Math) {
+            this.name = name;
+            this.Korean = Korean;
+            this.English = English;
+            this.Math = Math;
         }
 
-        // 정렬 조건 제시
         @Override
-        public int compareTo(Score o) {
-            if (this.langScore != o.langScore) {
-                return o.langScore - this.langScore;
+        public int compareTo(testForArr other) {
+            if (other.Korean == this.Korean) {
+                if (other.English == this.English) {
+                    if (other.Math == this.Math) {
+                        return this.name.compareTo(other.name);
+                    }
+                    return Integer.compare(other.Math, this.Math);
+                }
+                return Integer.compare(this.English, other.English);
             }
-            if (this.engScore != o.engScore) {
-                return this.engScore - o.engScore;
-            }
-            if (this.mathScore != o.mathScore) {
-                return o.mathScore - this.mathScore;
-            }
-            return str.compareTo(o.str);
+            return Integer.compare(other.Korean, this.Korean);
         }
     }
 }
