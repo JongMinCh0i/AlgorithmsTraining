8d6623a07e16ad509c8e9e4fafdd9a91c84cfc61
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-09-23 16:32:34 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1104 ms, Memory: 185480 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,47 +2,71 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
     static int N, M;
+    static int A[], A_exist[];
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        input();
+        logic();
+    }
 
+    static void input() throws IOException {
+        sb = new StringBuilder();
+        // 엔터를 기준으로 받습니다
+        br = new BufferedReader(new InputStreamReader(System.in));
+        // 첫번 째 엔터를 받은 문자열을 숫자로 변환합니다.
         N = Integer.parseInt(br.readLine());
+        // 다음 엔터를 통해서 받은 문자열을 공백을 기준으로 분리합니다.
+        st = new StringTokenizer(br.readLine());
+        // 숫자로 변환했던 수 만큼의 int[] 사이즈를 설정합니다.
+        A = new int[N];
 
-        Map<Integer, Integer> map = new HashMap<>();
-        // key 값에 입력받은 값을 
-        // value 값에 순차적으로 증가하는 int i 를 (0..1...2..3..4..5)
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
+        // 공백을 기준으로 분리했던 문자열을 숫자로 변환해서 배열에 추가했습니다.
         for (int i = 0; i < N; i++) {
-            int value = Integer.parseInt(st.nextToken());
-            // map.get(value) value를 통해 값을 입력 
-            // 입력했던 값이 존재 하지 않는다면 value, 1 로 저장
-            // 입력했던 값이 존재 했었다면 value, +1 로 저장
-            if (map.get(value) == null) {
-                map.put(value, 1);
-            } else {
-                map.put(value, map.get(value) + 1);
-            }
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-
+        M = Integer.parseInt(br.readLine());
+        A_exist = new int[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int input = Integer.parseInt(st.nextToken());
-            
-            // input을 통해 받은 value가 키(input)로 존재하는지 존재한다면 키를 바탕으로 value 삽입  
-            if (map.containsKey(input)) {sb.append(map.get(input)).append(" ");}
-            // 해당 키가 존재 하지 않는다면 " " 저장
-            else {sb.append(0).append(" "); }
-            
-//            if(map.get(input) == null) sb.append(0).append(" ");
-//            else sb.append(map.get(input)).append(" ");
+            A_exist[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(sb);
+    }
+
+    static int lower_bound(int A[], int L, int R, int A_exist) {
+        while (L < R) {
+            int mid = (L + R) / 2;
+            if (A[mid] >= A_exist) {
+                R = mid;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return R;
+    }
+
+    static int upper_bound(int A[], int L, int R, int A_exist) {
+        while (L < R) {
+            int mid = (L + R) / 2;
+            if (A[mid] > A_exist) {
+                R = mid;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return R;
+    }
+
+    static void logic() {
+        Arrays.sort(A);
+
+        for (int i = 0; i < M; i++) {
+            sb.append(upper_bound(A, 0, A.length, A_exist[i]) - lower_bound(A, 0, A.length, A_exist[i])).append(" ");
+        }
+        System.out.println(sb); // 출력값 : 7 0 0 3 0 0 0 0 / 정상적인 출력값 : 3 0 0 1 2 0 0 2
     }
 }
