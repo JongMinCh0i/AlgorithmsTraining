ad84f059b9de64ff28bf11da46fada6dcb13e928
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-10-04 11:39:41 +0900

    [Bronze IV] Title: Hello Judge, Time: 80 ms, Memory: 11380 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println("Hello World, Judge " + i + "!");
+        }
+    }
+}
