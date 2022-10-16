307f5957bc48521d280caeb4be239c0a934d3d00
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-10-16 09:40:35 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            if (N == 0 && M == 0)  break;
+
+            System.out.println(N + M);
+        }
+    }
+}
