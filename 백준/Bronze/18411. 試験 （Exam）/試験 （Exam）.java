00c8ca4bf3790a229b0ec662a16863078e92be0c
JongMinCh0i <80812697+JongMinCh0i@users.noreply.github.com>
2022-10-22 23:07:57 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 76 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[1] + arr[2]);
+    }
+}
