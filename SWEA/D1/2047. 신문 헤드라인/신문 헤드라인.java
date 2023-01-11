6a050eaeb28ac310226587a034f9b19e91da414e
tutti <80812697+JongMinCh0i@users.noreply.github.com>
2023-01-11 13:20:12 +0900

    [D1] Title: 신문 헤드라인, Time: 103 ms, Memory: 17,952 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class  Solution  {
+    static BufferedReader br;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        sb = new StringBuilder();
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb.append(br.readLine().toUpperCase());
+        System.out.println(sb);
+
+    }
+}
