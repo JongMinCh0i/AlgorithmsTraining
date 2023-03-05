147f8674b4cd4ce37f3a5f2283a5cfdea5edf492
tutti <80812697+JongMinCh0i@users.noreply.github.com>
2023-03-05 23:52:45 +0900

    [Bronze V] Title: 時間 (Hour), Time: 72 ms, Memory: 11440 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) * 24);
+    }
+}
