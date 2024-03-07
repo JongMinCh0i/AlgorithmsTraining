9d5ec4d29f1c60bb40d32d27ccfcefea3848dd9f
Tom <80812697+JongMinCh0i@users.noreply.github.com>
2024-03-07 16:22:15 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    private static final int ENDJOB = 100;
+
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+
+        int[] daysNeeded = new int[progresses.length];
+        for (int i = 0; i < progresses.length; i++) {
+            int remaining = ENDJOB - progresses[i];
+            daysNeeded[i] = remaining % speeds[i] == 0 ? remaining / speeds[i] : remaining / speeds[i] + 1;
+        }
+
+        int currentDay = 0;
+        int count = 0;
+        for (int i = 0; i < progresses.length; i++) {
+            if (daysNeeded[i] > currentDay) {
+                answer.add(count);
+                count = 1;
+                currentDay = daysNeeded[i];
+            } else {
+                count++;
+            }
+        }
+        answer.add(count);
+
+        // Remove the first element (0) as it's just a placeholder
+        answer.remove(0);
+
+        return answer;
+    }
+}
