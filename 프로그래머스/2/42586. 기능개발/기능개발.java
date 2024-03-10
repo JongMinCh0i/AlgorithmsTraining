d1bb0731e8433f0d79ed2e05436f60f19c7b382a
Tom <80812697+JongMinCh0i@users.noreply.github.com>
2024-03-10 09:01:24 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,31 +1,48 @@
-import java.io.*;
-import java.util.*;
-class Solution {
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
     private static final int ENDJOB = 100;
+    private static int cnt = 0;
+    private static int weight = 0;
 
     public List<Integer> solution(int[] progresses, int[] speeds) {
+        int[] jobArr = new int[progresses.length];
         List<Integer> answer = new ArrayList<>();
 
-        int[] daysNeeded = new int[progresses.length];
-        for (int i = 0; i < progresses.length; i++) {
-            int remaining = ENDJOB - progresses[i];
-            daysNeeded[i] = remaining % speeds[i] == 0 ? remaining / speeds[i] : remaining / speeds[i] + 1;
-        }
-
-        int currentDay = 0;
-        int count = 0;
-        for (int i = 0; i < progresses.length; i++) {
-            if (daysNeeded[i] > currentDay) {
-                answer.add(count);
-                count = 1;
-                currentDay = daysNeeded[i];
-            } else {
-                count++;
+        for (int i = 0; i < jobArr.length; i++) {
+            if (((ENDJOB - progresses[i]) % speeds[i]) == 0) {
+                jobArr[i] = ((ENDJOB - progresses[i]) / speeds[i]);
+            } else if ((ENDJOB - progresses[i]) % speeds[i] >= 1) {
+                jobArr[i] = (int) ((ENDJOB - progresses[i]) / speeds[i]) + 1;
             }
         }
-        answer.add(count);
-        answer.remove(0);
 
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = jobArr.length - 1; i >= 0; i--) {
+            stack.push(jobArr[i]);
+        }
+
+        while (!stack.isEmpty()) {
+            int jobDuty = stack.pop();
+            cnt++;
+            weight = (jobDuty);
+
+//            if (stack.isEmpty()) {
+//                answer.add(cnt);
+//                return answer;
+//            }
+
+            while (!stack.isEmpty() && stack.peek() <= weight) {
+                stack.pop();
+                cnt++;
+            }
+
+            answer.add(cnt);
+            cnt = 0;
+        }
         return answer;
     }
 }
