6931763e84c235fc291ae6682597e1cfc0be7873
Tom <80812697+JongMinCh0i@users.noreply.github.com>
2024-04-01 17:13:08 +0900

    [level 1] Title: 폰켓몬, Time: 6.79 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        
+        int N = nums.length / 2; 
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        
+        return Math.min(N, set.size());
+    }
+}
