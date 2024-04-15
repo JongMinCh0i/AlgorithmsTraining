33ab976b362ced97e281fba94af9b4a9a5015f43
Tom <80812697+JongMinCh0i@users.noreply.github.com>
2024-04-15 13:26:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.86 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,32 +1,32 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
+    public static HashMap<String, Integer> participantMap = new HashMap<>();
     
-    public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> map = new HashMap<>();
-        
-         for (int i = 0; i < participant.length; i++) {
-            if (!map.containsKey(participant[i])) {
-                map.put(participant[i], 1);
-            } else {
-                map.put(participant[i], map.get(participant[i]) + 1);
-            }
+    public void init(String[] participant) {
+        for(int i = 0; i < participant.length; i++) {
+            String name = participant[i];
+            participantMap.put(name , participantMap.getOrDefault(name, 0) + 1);
         }
-        
-         for (int j = 0; j < completion.length; j++) {
-            if (map.containsKey(completion[j])) {
-                map.put(completion[j], map.get(completion[j]) - 1);
-            }
+    }
+    
+    public String logic(String[] participant, String[] completion) {
+        for(int i = 0; i < completion.length; i++) {
+            String completionName = completion[i];
+            participantMap.put(completionName, participantMap.get(completionName) - 1);
         }
         
         for(int i = 0; i < participant.length; i++) {
-            if(map.get(participant[i]) > 0) {
-                return participant[i];
+            String name =  participant[i];
+            if(participantMap.get(name) > 0) {
+                return name;
             }
-            
         }
-
         return "";
+    }
+
+    public String solution(String[] participant, String[] completion) {
+        init(participant);
+        return logic(participant, completion);
     }
 }
