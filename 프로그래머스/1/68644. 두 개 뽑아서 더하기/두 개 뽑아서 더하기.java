1d037c6b5a37d31dc2b97a8363ca11021badf147
Tom <80812697+JongMinCh0i@users.noreply.github.com>
2024-11-17 20:47:44 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.86 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -3,16 +3,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        HashSet<Integer> hs = new HashSet<>();
-    
-        for(int i = 0; i < numbers.length - 1; i++) {
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        
+        // numbers.length를 돌면서 i 값은 늘 다음 인덱스를 가리킨다.
+        for(int i = 0; i < numbers.length; i++) {
+            int num1 = numbers[i];            
+            
             for(int j = i + 1; j < numbers.length; j++) {
-                hs.add(numbers[i] + numbers[j]);
-            }
+                int num2 = numbers[j];
+                list.add(num1 + num2);
+           }
         }
         
-        return hs.stream().sorted().mapToInt(Integer::intValue).toArray();
+        // list의 size만큼 돌면서 해당 값들을 set 자료구조에 더한다.
+        for(int i = 0; i < list.size(); i++) {
+            set.add(list.get(i));
+        }
         
+        // 중복이 제거된 set의 길이 만큼 정수형 배열을 선언한다.
+        int[] answer = new int[set.size()];
+        
+        // 이를 직접 하나씩 돌면서 위에 선언한 정수형 배열에 값을 넣고 반환한다.
+        Iterator<Integer> iterator = set.iterator();
+        for(int i = 0; i < set.size(); i++) {
+            answer[i] = iterator.next();
+        }
 
+        
+        Arrays.sort(answer);
+        
+        return answer;
     }
 }
