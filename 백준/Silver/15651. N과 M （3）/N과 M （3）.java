4709134c0b8b895aa2e4ca375df750d4f5995487
Tom <80812697+JongMinCh0i@users.noreply.github.com>
2025-06-21 15:46:49 +0900

    [Silver III] Title: N과 M (3), Time: 380 ms, Memory: 68176 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,25 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-    static StringBuilder sb = new StringBuilder();
 
-    static void re_fun(int n) {
-        if (n == M + 1) { // 완전 탐색 종료
-            for (int i = 1; i <= M; i++) sb.append(arr[i]).append(' ');
-            sb.append('\n');
-        } else {
-            for (int i = 1; i <= N; i++) {
-                arr[n] = i;
-
-                re_fun(n + 1);
-            }
-        }
-    }
+    public static int N, M;
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] selected;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,10 +14,23 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M + 1];
+        selected = new int[M];
+        rec_func(0);
+        System.out.println(sb);
+    }
 
-        re_fun(1);
+    static void rec_func(int k) {
+        if (k == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
 
-        System.out.print(sb.toString());
+        for (int i = 1; i <= N; i++) {
+            selected[k] = i;
+            rec_func(k + 1);
+        }
     }
 }
